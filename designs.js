6cb4b073da4b5b5ce21261b93ab9d0fe0b7ec60f
Alejandro-Sierra <alexsierra1598@gmail.com>
2018-10-21 23:14:59 -0400

    Initial commit in pixel art app

---
+++ designs.js
@@ -0,0 +1,36 @@
+//values assigned
+let height, width, color;
+
+//if sizePicker is clicked get values of height and width and store them
+$('#sizePicker').submit(function (event) {
+    event.preventDefault();
+    height = $('#inputHeight').val();
+    width = $('#inputWidth').val();
+    makeGrid(height, width);
+})
+
+// When size is submitted by the user, call makeGrid()
+
+//automatically removes the last grid when run
+function makeGrid(x, y) {
+  $('tr').remove();
+
+  for (let i = 1; i <= x; i++) {
+      $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+
+      for (let j = 1; j <= y; j++) {
+          $('#table' + i).append('<td></td>');
+      }
+  }
+
+
+//adding color
+//if td is clicked run the funciton of getColor
+$('td').click(function getColor () {
+    color =  $('#colorPicker').val();
+//deleted the old one > if there was already a color then it would go back to white and then click again for new color
+    $(this).attr('style', 'background-color:' + color);
+
+});
+
+}
